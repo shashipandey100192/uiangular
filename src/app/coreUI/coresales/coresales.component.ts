625ec9551fd7d5ea0bda87f61784e99f4adfebc8
windows <windows@microsoft.com>
2022-12-18 14:58:01 +0530

    pipe

--- src/app/coreUI/coresales/coresales.component.ts
+++ src/app/coreUI/coresales/coresales.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CoresalesComponent {
 
+  a:string ="ngular is a TypeScript-based, free and open-source web application framework led by the Angular Team at Google and by a community of individuals and corporations. Angular is a complete re"
+b:number=500;
+
+mydate= new Date().getTime();
+
+
 }
